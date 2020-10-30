cf31a7c02a5a73d8c7628885fa3463063110e59c
mechienct <mechien@gmail.com>
2020-10-29 21:04:44 -0700

    Update lanner_web_main.py

--- lanner_web_main.py
+++ lanner_web_main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("Lanner USA spec text to SKU number")
-user_input = st.text_input("Input text (Press \"Enter\"): ")
+user_input = st.text_input("Enter your spec text and Press \"Enter\": ")
 if user_input:
   subtxt = user_input.split(':')
   op =  subtxt[0].split(' - ')[1].split(' ')[0]
