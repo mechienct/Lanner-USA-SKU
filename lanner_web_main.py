68355139b2bf5483efdf7932a16076cc32de111d
mechienct <mechien@gmail.com>
2020-10-29 20:46:01 -0700

    Update lanner_web_main.py

--- lanner_web_main.py
+++ lanner_web_main.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.title("my first 88app")
+st.title("Lanner USA spec text to SKU number")
+user_input = st.text_input("Input text:")
+subtxt = user_input.split(':')
+op =  subtxt[0].split(' - ')[1].split(' ')[0]
+for i in range(1,len(subtxt),1):
+  op += '-'
+  op += subtxt[i].split('-')[0].strip().split(' ')[0]
+st.text("SKU number: ")
+st.text(op)
